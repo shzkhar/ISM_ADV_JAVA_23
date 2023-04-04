b06f03126a906b46f683ee7b72f8074157b09e7c
shzkhar <shekhar98h@gmail.com>
2023-04-04 21:34:49 +0530

    init

--- Adv_Java_Crud/src/main/java/com/bean/UserBean.java
+++ Adv_Java_Crud/src/main/java/com/bean/UserBean.java
@@ -6,4 +6,30 @@ public class UserBean {
 	private String firstname;
 	private String email;
 	private String password;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
